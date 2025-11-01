d0af710f5c550b367161a3601510fad23be3ceb5
Ibrahim Sheta <hemasheta061@gmail.com>
2025-11-01 19:32:00 +0200

    Changes

--- Depi_Project/Depi_Project/obj/Debug/net8.0/Depi_Project.AssemblyInfo.cs
+++ Depi_Project/Depi_Project/obj/Debug/net8.0/Depi_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Depi_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35cc20c8a1fc3e8ea4d7ead862f5b39cf9e1549")]
 [assembly: System.Reflection.AssemblyProductAttribute("Depi_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Depi_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
