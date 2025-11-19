0e7734509d84b575150751fbfd51101a2af42f73
Ibrahim Sheta <hemasheta061@gmail.com>
2025-11-19 20:02:20 +0200

    Views Tweaks

--- Depi_Project/Depi_Project/obj/Debug/net8.0/Depi_Project.AssemblyInfo.cs
+++ Depi_Project/Depi_Project/obj/Debug/net8.0/Depi_Project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Depi_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abf4c3abb1f66b9871e2f04e0a072aac77b4540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4e20e5494b5d489b3f99adaf522c05c5a56e8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Depi_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Depi_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
